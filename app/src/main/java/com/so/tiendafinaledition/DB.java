0210700716abb855c1e7d9caedb534358de727bd
Carlos-Larin <157402737+Carlos-Larin@users.noreply.github.com>
2024-04-11 21:36:06 -0600

    Update DB.java

--- app/src/main/java/com/so/tiendafinaledition/DB.java
+++ app/src/main/java/com/so/tiendafinaledition/DB.java
@@ -1,5 +1,4 @@
 package com.so.tiendafinaledition;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,11 +9,11 @@ import androidx.annotation.Nullable;
 public class DB extends SQLiteOpenHelper {
     private static final String dbname = "productos";
     private static final int v = 1;
-    private static final String SQLdb = "CREATE TABLE productos(idProducto integer primary key autoincrement, " +
+    private static final String SQLdb = "CREATE TABLE productos(id text, rev text, idProducto integer primary key autoincrement, " +
             "codigo text, descripcion text, marca text, presentacion text, precio text, foto text)";
 
-    public DB(@Nullable Context context, String s, Object o, int i) {
-        super(context, dbname, null, v);
+    public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, dbname, factory, v);
     }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
@@ -30,13 +29,13 @@ public class DB extends SQLiteOpenHelper {
             SQLiteDatabase db = getWritableDatabase();
             String sql = "";
             if (accion.equals("nuevo")) {
-                sql = "INSERT INTO productos(marca, descripcion, presentacion, precio, foto) VALUES('" + datos[1] + "', '" +
-                        datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "' )";
+                sql = "INSERT INTO productos(id,rev,idProducto,marca, descripcion, presentacion, precio, foto) VALUES('" + datos[0] + "', '" +
+                        datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "' , '" + datos[5] + "', '" + datos[6] + "', '" + datos[7] + "')";
             } else if (accion.equals("modificar")) {
-                sql = "UPDATE productos SET marca='" + datos[1] + "', descripcion='" + datos[2] + "', presentacion='" + datos[3] + "', " +
-                        "precio='" + datos[4] + "', foto='" + datos[5] + "' WHERE idProducto='" + datos[0] + "'";
+                sql = "UPDATE productos SET id='"+ datos[0] +"',rev='"+ datos[1] +"',marca='" + datos[3] + "', descripcion='" + datos[4] + "', presentacion='" + datos[5] + "', " +
+                        "precio='" + datos[6] + "', foto='" + datos[7] + "' WHERE idProducto='" + datos[2] + "'";
             } else if (accion.equals("eliminar")) {
-                sql = "DELETE FROM productos WHERE idProducto='" + datos[0] + "'";
+                sql = "DELETE FROM productos WHERE idProducto='" + datos[2] + "'";
             }
             db.execSQL(sql);
             return "ok";
