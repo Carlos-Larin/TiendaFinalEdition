8cf707a305ce76335972fd27ac56e7cb801bcc2e
Carlos-Larin <157402737+Carlos-Larin@users.noreply.github.com>
2024-04-20 10:41:21 -0600

    Update utilidades.java

--- app/src/main/java/com/so/tiendafinaledition/utilidades.java
+++ app/src/main/java/com/so/tiendafinaledition/utilidades.java
@@ -6,8 +6,8 @@ import java.util.Base64;
 import java.util.UUID;
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class utilidades {
-    static String urlConsulta="http://192.168.1.20:5984/tiendaonline/_design/TiendaOnline/_view/TiendaOnline";
-    static String urlMto="http://http://192.168.1.20:5984/tiendaonline";
+    static String urlConsulta="http://192.168.1.17:5984/tiendaonline/_design/TiendaOnline/_view/TiendaOnline";
+    static String urlMto="http://192.168.1.17:5984/tiendaonline";
     static String user="CarlosAristides";
     static String passwd="CARTOS12A?";
     static String credencialesCodificadas = Base64.getEncoder().encodeToString((user +":"+ passwd).getBytes());
